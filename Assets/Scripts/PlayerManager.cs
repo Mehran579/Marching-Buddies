b48ee94014775a595663e0a7988179d2c2aa9edf
Mehran69696 <mehran810428@gmail.com>
2026-07-08 00:02:56 +0530

    Added player movement and jump and head swap
    coyote time, input jump buffer, right left moving action , player front head swap mechanic >.<

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,16 +1,110 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Rigidbody2D playerrb;
+    [Header("Stats")]
+    public float MoveSpeed;
+    public float Jump;
+    [Header("Coyote time")]
+    public float coyotetime;
+    float coyotetimer;
+    [Header("Jump input buffer")]
+    public float jumpinput_time;
+    float jumpinput_timer;
+    [Space(10)]
+    Vector2 movement;
+    [Header("Ground Check")]
+    public float raycastradius;
+    public float castdistance;
+    public LayerMask groundlayer;
+    #region Player Inputs
+    public void OnMove(InputAction.CallbackContext context)       //Movement input
     {
-        Debug.Log("woohoo");
+        movement = context.ReadValue<Vector2>();
+    }
+    public void OnJump(InputAction.CallbackContext context)       //Jump Input
+    {
+        if (context.performed)
+        {
+            jumpinput_timer = jumpinput_time;
+            if (coyotetimer > 0) 
+            {
+                jump();
+            }
+        }
+        else if (context.canceled && playerrb.linearVelocity.y > 0) 
+        {
+            playerrb.linearVelocity = new Vector2(playerrb.linearVelocity.x, playerrb.linearVelocity.y * 0.5f);
+            coyotetimer = 0;
+        }
+    }
+    public void OnShift(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            swaplocations();
+        }
+    }
+    #endregion
+    private void FixedUpdate()
+    {
+        playerrb.linearVelocity = new Vector2(movement.x * MoveSpeed, playerrb.linearVelocity.y);
+        if (isGrounded())
+        {
+            coyotetimer = coyotetime;
+            if(jumpinput_timer > 0)
+            {
+                jump();
+            }
+        }
+        else if (!isGrounded())
+        {
+            coyotetimer -= Time.deltaTime;
+        }
+        jumpinput_timer -= Time.deltaTime;
+    }
+    void jump()
+    {
+        coyotetimer = 0;
+        jumpinput_timer = 0;
+        playerrb.linearVelocity = new Vector2(playerrb.linearVelocity.x, Jump);
+
+    }
+    #region Ground Check
+    public bool isGrounded()
+    {
+        if (Physics2D.CircleCast(transform.position, raycastradius, -transform.up, castdistance, groundlayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.DrawWireSphere(transform.position - transform.up * castdistance, raycastradius);
     }
+    #endregion
+    #region Head location swapper
+    void swaplocations()
+    {
+        foreach(Transform child in transform)
+        {
+            if(child.localPosition.x == 2)
+            {
+                child.localPosition = new Vector2(-2, child.localPosition.y);
+            }
+            else
+            {
+                child.localPosition = new Vector2(child.localPosition.x + 1, child.localPosition.y);
+            }
+        }
+    }
+    #endregion
 }
+
