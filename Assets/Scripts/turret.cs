d4cf6fa58fff9b3672e58235deecb26c2e318e3a
Mehran69696 <mehran810428@gmail.com>
2026-07-10 20:44:43 +0530

    added debug

--- Assets/Scripts/turret.cs
+++ Assets/Scripts/turret.cs
@@ -7,6 +7,7 @@ public class turret : MonoBehaviour
     {
         if (other.transform.parent.CompareTag("Player") && !other.transform.parent.GetComponent<PlayerManager>().ishidden)
         {
+            Debug.Log("why");
             transform.localScale = new Vector3(-1, 1, 1);
             GameObject nmissile = Instantiate(missile,transform.position,Quaternion.identity);
             nmissile.GetComponent<Rigidbody2D>().AddForce((other.transform.position - transform.position).normalized * 14f, ForceMode2D.Impulse);
